d2dc47ba087776644a35b53c42a63f2e8645ce7b
AnushriDubey <89227101+AnushriDubey@users.noreply.github.com>
2021-08-20 16:19:22 +0530

    Create PaintingTheFence.java

---
+++ PaintingTheFence.java
@@ -0,0 +1,16 @@
+public class PaintingTheFence {
+    public static long paintFence(int n, int k) {
+        long same = k;
+        long diff = k*(k-1);
+        long total = same + diff;
+        for(int i=3;i<=n;i++) {
+            same = diff;
+            diff = total*(k-1);
+            total = same + diff;
+        }
+        return total;
+    }
+    public static void main(String args[]) {
+
+    }
+}
